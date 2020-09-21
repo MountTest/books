f225f9f226fe1d13161ec556f308ceceff577e53
Frogy <nurs.axe@gmail.com>
2020-09-21 13:43:48 +0600

    Revert "web"
    This reverts commit b330e2131ae21e46e179615183ad157063958204.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function () {
-    return view('layouts.app');
-})->where('any', '.*');
-
+Route::get('/', function () {
+    return view('app');
+})->name('welcome');
 
 Auth::routes();
 
